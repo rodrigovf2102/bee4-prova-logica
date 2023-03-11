a8d740ebecd0e87bbd33298baac8bd16b21104c7
Rodrigo <rodrigovf2102@gmail.com>
2023-03-11 10:19:39 -0300

    perf: bubbleSort algorithm improved, know it toggles to ascending-descending when using the same file

--- TAREFA2/Program.js
+++ TAREFA2/Program.js
@@ -11,9 +11,15 @@ async function readMapa(){
 }
 
 function sortMapa(cities){
+  let order;
+  let toggle;
+  if(cities[0]>cities[1]) toggle = false;
+  if(cities[0]<cities[1]) toggle = true; 
   for(let i=1;i<cities.length;i++){
     for(let j=1;j<cities.length;j++){
-      if(cities[j]<cities[j+1]){
+      if(toggle) order = cities[j] >cities[j+1];
+      if(!toggle) order = cities[j] < cities[j+1];
+      if(order){
         const aux = cities[j];
         cities[j] = cities[j+1];
         cities[j+1] = aux;

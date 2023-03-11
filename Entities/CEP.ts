dbd1fe6e497b625f14e05bbbe105e604418d581a
Rodrigo <rodrigovf2102@gmail.com>
2023-03-11 12:43:20 -0300

    refactor: TAREFA3 know use OOP to solve the problem

--- Entities/CEP.ts
+++ Entities/CEP.ts
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export class CEP {
   cep : string;
   logradouro? : string;
@@ -10,23 +12,75 @@ export class CEP {
   gia? : string;
   cepInvalido? : string;
 
-  constructor(cep : string){
-    this.cep = cep;
+  constructor(cep : string, logradouro? : string, complemento? : string, bairro? : string, localidade?: string, uf? : string, unidade? : string, ibge? : string, gia? : string){
+    if(cep)this.cep = cep;
+    if(logradouro)this.logradouro = logradouro;
+    if(!logradouro) this.logradouro ="";
+    if(complemento)this.complemento = complemento;
+    if(!complemento) this.complemento ="";
+    if(bairro)this.bairro = bairro;
+    if(!bairro) this.bairro ="";
+    if(localidade)this.localidade = localidade;
+    if(!localidade) this.localidade ="";
+    if(uf)this.uf = uf;
+    if(!uf) this.uf ="";
+    if(unidade)this.unidade = unidade;
+    if(!unidade) this.unidade ="";
+    if(ibge)this.ibge = ibge
+    if(!ibge) this.ibge ="";
+    if(gia)this.gia = gia;
+    if(!gia) this.gia ="";
   }
 
   async getCEPInfo(){
-    if(!this.verifyCEP) return this.cepInvalido = "CEP Inválido";
+    if(!this.verifyCEP()) return this.cepInvalido = "CEP Inválido";
     try {
       const response = await fetch(`https://viacep.com.br/ws/${this.cep}/json/`);
-      const data : viacep = await response.json();
-      return data;
+      const data : any = await response.json();
+      const viaCepData : viacep = {};
+      if(data.cep) viaCepData.cep = data.cep;
+      if(data.logradouro) viaCepData.logradouro = data.logradouro;
+      if(data.complemento) viaCepData.complemento = data.complemento;
+      if(data.bairro) viaCepData.bairro = data.bairro;
+      if(data.localidade) viaCepData.localidade =data.localidade;
+      if(data.uf) viaCepData.uf = data.uf;
+      if(data.ibge) viaCepData.ibge = data.ibge;
+      if(data.gia) viaCepData.gia = data.gia;
+      if(data.ddd) viaCepData.ddd = data.ddd;
+      if(data.siafi) viaCepData.siafi = data.siafi;
+      if(data?.erro) viaCepData.erro = data.erro;
+      return viaCepData;
     } catch (error) {
       console.log(error);
     }
   }
 
-  static arrayToCEP(){
-    
+  static csvToCEPs(csvCEPs : Buffer) : CEP[] {
+    const arrayCEPs = csvCEPs.toString().split('\n').map(cep=>cep.split(';'));
+    const ceps : CEP[] = [];
+    for (const arrayCEP of arrayCEPs) {
+      let cep : CEP;
+      if(arrayCEP[1]==="") cep = new CEP(arrayCEP[0]);
+      if(arrayCEP[1]!=="") cep = new CEP(arrayCEP[0],arrayCEP[1],arrayCEP[2],arrayCEP[3],arrayCEP[4],arrayCEP[5],arrayCEP[6],arrayCEP[7],arrayCEP[8]);
+      ceps.push(cep);
+    }
+    return ceps;
+  }
+
+  static arrayToString(ceps : CEP[]) : string {
+    console.log(ceps);
+    let cepsString : string[] = []
+    for (let i=0; i< ceps.length; i++) {
+      const stringCep = CEP.cepToString(ceps[i]);
+      cepsString.push(stringCep);
+    }
+    console.log(cepsString)
+    return cepsString.join("\n");
+  }
+
+  static cepToString(cep : CEP) : string{
+    if(!cep.cepInvalido) return `${cep.cep};${cep.logradouro};${cep.complemento};${cep.bairro};${cep.localidade};${cep.uf};${cep.unidade};${cep.ibge};${cep.gia}`;
+    return `${cep.cep};${cep.cepInvalido}`
   }
 
   verifyCEP(){
@@ -36,29 +90,33 @@ export class CEP {
   }
 
   async setCEPInfo(){
+    if(this.cep==="CEP") return;
     const data = await this.getCEPInfo() as viacep;
-    if(data.erro) return this.cepInvalido = "CEP Inválido"
-    this.bairro = data.bairro;
-    this.logradouro = data.logradouro;
-    this.complemento = data.complemento;
-    this.localidade = data.localidade;
-    this.uf = data.uf;
-    this.unidade = data.ddd;
-    this.ibge = data.ibge
-    this.gia = data.gia;
+    if(data?.erro){
+      this.cepInvalido = "CEP Inválido";
+      return
+    }
+    if(data.bairro)this.bairro = data.bairro;
+    if(data.logradouro)this.logradouro = data.logradouro;
+    if(data.complemento)this.complemento = data.complemento;
+    if(data.localidade)this.localidade = data.localidade;
+    if(data.uf)this.uf = data.uf;
+    if(data.ddd)this.unidade = data.ddd;
+    if(data.ibge)this.ibge = data.ibge
+    if(data.gia)this.gia = data.gia;
   }
 }
 
 type viacep = {
-  cep : string,
-  logradouro : string,
-  complemento : string,
-  bairro : string,
-  localidade: string,
-  uf:string,
-  ibge:string,
-  gia:string,
-  ddd:string,
-  siafi:string,
+  cep? : string,
+  logradouro? : string,
+  complemento? : string,
+  bairro? : string,
+  localidade?: string,
+  uf?:string,
+  ibge?:string,
+  gia?:string,
+  ddd?:string,
+  siafi?:string,
   erro?:boolean
 }
--- TAREFA3/Program.ts
+++ TAREFA3/Program.ts
@@ -1,58 +1,23 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
+import { CEP } from '../Entities/CEP.js';
 import { FileService } from "../Services/FileService.js";
 
 async function readCEPs(){
-  try {
-    const data = await FileService.readCSV("CEP.csv");
-    const cepsArray = data.toString().split('\n').map(cep=>cep.split(';'));
-
+    const data = await FileService.readCSV("CEPs.csv");
+    const ceps = CEP.csvToCEPs(data);
     return ceps;
-  } catch (error) {
-    console.log("Erro ao ler o arquivo");
-  }
 }
 
-function writeCEPs(ceps){
-  fs.writeFile('CEPsWithInfoCopie.csv', ceps, (error)=>{ if(error) throw error });
-}
-
-async function insertCEPsInfo(ceps){
-  for(let i=1; i < ceps.length; i++){
-    const validCep = verifyCEP(ceps[i][0]);
-    if(validCep) {
-      const data = await getCEPInfo(validCep);
-      ceps[i] = [ceps[i][0],data.logradouro, data.complemento, data.bairro, data.localidade, data.uf, data.ddd, data.ibge, data.gia]
-      ceps[i] = ceps[i].join(';');
-    }
-    if(!validCep){
-      ceps[i] = [ceps[i][0],"CEP Inválido"];
-      ceps[i] = ceps[i].join(';');
-    }
-  }
-  return ceps.join('\n');
-}
-
-async function getCEPInfo(cep){
-  try {
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log("Erro ao reber informações da api VIACEP.");
-  }
-}
-
-function verifyCEP(cep){
-  cep = cep.replace(/\D/g,'');
-  if(cep.length===8) return cep;
-  return false;
+function writeCEPs(ceps : CEP[]){
+  let cepsString = CEP.arrayToString(ceps);
+  FileService.writeCSV(cepsString,"CEPsWithInfo.csv")
 }
 
 async function solver(){
   const ceps = await readCEPs();
-  const cepsWithInfo = await insertCEPsInfo(ceps);
-  writeCEPs(cepsWithInfo);
+  for(let i =0 ; i < ceps.length ; i++){
+    await ceps[i].setCEPInfo();
+  }
+  writeCEPs(ceps);
 }
 
 solver();
